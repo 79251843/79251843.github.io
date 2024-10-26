653781d7cb03a2ac73416dd68ec16e3f40295062
goodboy010 <413791664@qq.com>
2024-10-26 19:58:04 +0800

    Site updated: 2024-10-26 19:58:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/My-New-Post/","2024/10/26/hello-world/"];function toRandomPost(){
+var posts=["2024/10/26/这是一篇新的博文-1/","2024/10/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
